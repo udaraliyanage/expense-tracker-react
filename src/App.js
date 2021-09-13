b3231764882ba03558edcd3b1a17233e6142a059
udaraliyanage <udaraliyanage@gmail.com>
2021-09-13 13:54:13 +0800

    inner components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpensePanel from './components/ExpensePanel';
 
 function App() {
+
+  const items = [
+      {
+          description : 'Singleton',
+          date : new Date(2021, 8, 22),
+          amount : 200
+      },
+      {
+          description : 'Bite',
+          date : new Date(2021, 8, 5),
+          amount : 20
+      },
+      {
+          description : 'Cab',
+          date : new Date(2021, 8, 8),
+          amount : 14
+      },
+      {
+          description : 'Icecube',
+          date : new Date(2021, 8, 14),
+          amount : 2
+      },
+      {
+          description : 'Ciga',
+          date : new Date(2021, 8, 14),
+          amount : 14
+      }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div>
+        <ExpensePanel items={items}/>
+
+      </div>
   );
 }
 
---
+++ src/components/ExpensePanel.js
@@ -0,0 +1,22 @@
+import './ExpensePanel.css'
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+
+function ExpensePanel(props) {
+    var expenseItems = props.items.map(expense =>
+        (
+            <ExpenseItem
+                title={expense.description}
+                date={expense.date}
+                amount={expense.amount}
+            />
+        )
+    );
+    return (
+        <Card className="expenses">
+            {expenseItems}
+        </Card>
+    )
+}
+
+export default ExpensePanel
