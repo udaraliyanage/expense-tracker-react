08ece8db4f3609fdd06f4140f2f85e5a1d6d4a54
udaraliyanage <udaraliyanage@gmail.com>
2021-09-15 14:14:46 +0800

    Add dynamic expenses

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+
+    function saveExpenseHandler(enteredExpenseData) {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        props.onAddExpense(expenseData)
+    }
+
+    return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveNewExpense={saveExpenseHandler}/>
+    </div>
+  );
+};
+
+export default NewExpense;
