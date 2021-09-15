7b1f6e2b57204afe8655e68925afed0a55086435
udaraliyanage <udaraliyanage@gmail.com>
2021-09-15 14:14:46 +0800

    Add dynamic expenses

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import ExpensePanel from './components/Expenses/ExpensePanel';
+import NewExpense from "./components/NewExpense/NewExpense";
+import React, {useState} from 'react';
 
 function App() {
 
-  const items = [
-      {
-          description : 'Singleton',
-          date : new Date(2021, 8, 22),
-          amount : 200
-      },
-      {
-          description : 'Bite',
-          date : new Date(2021, 8, 5),
-          amount : 20
-      },
-      {
-          description : 'Cab',
-          date : new Date(2021, 8, 8),
-          amount : 14
-      },
-      {
-          description : 'Icecube',
-          date : new Date(2021, 8, 14),
-          amount : 2
-      },
-      {
-          description : 'Ciga',
-          date : new Date(2021, 8, 14),
-          amount : 14
-      }
-  ]
-  return (
+    let expenses = [
+        {
+            description: 'Singleton',
+            date: new Date(2021, 8, 22),
+            amount: 200
+        },
+        {
+            description: 'Bite',
+            date: new Date(2021, 8, 5),
+            amount: 20
+        },
+        {
+            description: 'Cab',
+            date: new Date(2021, 8, 8),
+            amount: 14
+        },
+        {
+            description: 'Icecube',
+            date: new Date(2021, 8, 14),
+            amount: 2
+        },
+        {
+            description: 'Ciga',
+            date: new Date(2021, 8, 14),
+            amount: 14
+        }
+    ]
 
-      <div>
-        <ExpensePanel items={items}/>
+    const [items, setItems] = useState(expenses)
+    const addExpenseHandler = expense => {
+        console.log("#Main App###")
+        console.log(expense)
+        const e = {
+            description: 'New Item',
+            date: new Date(2021, 8, 22),
+            amount: 200
+        }
 
-      </div>
-  );
+        setItems([...items, e])
+    };
+
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <ExpensePanel items={items}/>
+        </div>
+    );
 }
 
 export default App;
