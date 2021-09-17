8a1a6bdb5cd60b6589d7e66dc3320695322a9250
udaraliyanage <udaraliyanage@gmail.com>
2021-09-17 17:49:47 +0800

    Add expense filter which filters by year

--- src/components/Expenses/ExpensePanel.js
+++ src/components/Expenses/ExpensePanel.js
@@ -1,23 +1,35 @@
 import './ExpensePanel.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 function ExpensePanel(props) {
-    var expenseItems = props.items.map(expense =>
-        (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.description}
-                date={expense.date}
-                amount={expense.amount}
-            />
-        )
-    );
+
+    const [selectedYear, setFilteredYear] = useState('2020')
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    let expenseItems = props.items
+        .filter(i => i.date.getFullYear().toString() === selectedYear)
+        .map(expense =>
+            (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.description}
+                    date={expense.date}
+                    amount={expense.amount}
+                />
+            )
+        );
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={selectedYear} onChangeFilter={filterChangeHandler}/>
             {expenseItems}
         </Card>
     )
 }
+
 
 export default ExpensePanel
